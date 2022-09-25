f7d4dbf5ede07ce1e4d00f4c11ca9ba2eb4108c9
Victor Villazón <victorzn23@gmail.com>
2022-09-25 06:25:15 -0500

    feat: work with express

--- index.js
+++ index.js
@@ -1,18 +1,64 @@
-const http = require('http')
+const express = require('express')
+const nodemon = require('nodemon')
+const app = express()
 
-const fruits = [
-  { id: 1, name: "apple"},
-  { id: 2, name: "orange"},
-  { id: 3, name: "banana"},
-]
-
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' })
-  res.end(JSON.stringify(fruits))
-})
+app.use(express.json())
 
 const PORT = 3001
 
-app.listen(PORT)
+const notes = [
+  { id: 1, content: "Note #1", date: "2022-09-24T15:04:22.098Z", important: true},
+  { id: 2, content: "Note #2", date: "2022-09-24T15:04:22.098Z", important: true},
+  { id: 3, content: "Note #3", date: "2022-09-24T15:04:22.098Z", important: true},
+]
 
-console.log(`Server running on port ${PORT}`)
+app.get('/', (req, res) => {
+  res.send('<h1>Hello API</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+  const id  = Number(req.params.id)
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id  = Number(req.params.id)
+  notes = notes.filter(note => note.id !== id)
+  res.status(204).end()
+})
+
+app.post('/api/notes', (req, res) => {
+  const note = request.body
+
+  if (!note || !note.content) {
+    return res.status(400).json({ error: 'note.content is missing' })
+  }
+
+  const ids = notes.map(note => note.id)
+  const maxId = Math.max(...ids)
+
+  const important = typeof note.important !== 'undefined' ? note.important : false
+
+  const newNote = {
+    id: maxId + 1,
+    content: note.content,
+    important,
+    date: new Date().toISOString()
+  }
+
+  // notes = [...notes, newNote]
+  notes = notes.concat(newNote)
+
+  res.json(notes)
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
